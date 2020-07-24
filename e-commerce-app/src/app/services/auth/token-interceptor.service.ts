eb27ba06cf1fa18f059b2526c81a9469086e5230
remah massri <remah.massri@gmail.com>
2020-07-24 14:44:35 +0300

    added token interceptor service

--- e-commerce-app/src/app/services/auth/token-interceptor.service.ts
+++ e-commerce-app/src/app/services/auth/token-interceptor.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable, Injector} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService {
+export class TokenInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+
+  constructor(  private injector :Injector) { }
+
+  // Interceptor are not dependable class so we need to use injector to hold the required dependencies ..
+
+  intercept(req: HttpRequest<any>, next: HttpHandler){
+    const authService = this.injector.get(AuthService);
+    const tokenReq = req.clone({
+      setHeaders :{
+        Authorization : `Bearer ${authService.getToken()}`,
+      }
+    });
+    return next.handle(tokenReq);
+  }
 }
